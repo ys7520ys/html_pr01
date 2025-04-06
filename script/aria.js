a4c6e23cc7e426ae4aad9b07b283eb269b92f187
yeseong Kim <ys7520ys@naver.com>
2025-04-06 21:52:57 +0900

    fifthCmt

--- script/aria.js
+++ script/aria.js
@@ -1,24 +1,23 @@
 function setSidebarFocusable(isOpen) {
-    const sidebar = document.querySelector('.sidebar');
-    const focusableEls = sidebar.querySelectorAll('a, button, input, select, textarea, [tabindex]');
-    const menuExpanded = document.querySelector('.menuBtn');
+  const sidebar = document.querySelector('.sidebar');
+  const focusableEls = sidebar.querySelectorAll('a, button, input, select, textarea, [tabindex]');
+  const menuExpanded = document.querySelector('.menuBtn');
 
-    focusableEls.forEach(el => {
-      if (isOpen) {
-        el.removeAttribute('tabindex'); 
-        menuExpanded.setAttribute('aria-expanded','true');
-      } else {
-        el.setAttribute('tabindex', '-1'); 
-        menuExpanded.setAttribute('aria-expanded','false');
-      }
-    });
-  }
-  
-  // 예시: 사이드바 열 때/닫을 때 호출
-  document.querySelector('.menuBtn').addEventListener('click', () => {
-    setSidebarFocusable(true);
+  focusableEls.forEach(el => {
+    if (isOpen) {
+      el.removeAttribute('tabindex'); 
+      menuExpanded.setAttribute('aria-expanded','true');
+    } else {
+      el.setAttribute('tabindex', '-1'); 
+      menuExpanded.setAttribute('aria-expanded','false');
+    }
   });
-  document.querySelector('.menuCloseBtn').addEventListener('click', () => {
-    setSidebarFocusable(false);
-  });
+}
+
+document.querySelector('.menuBtn').addEventListener('click', () => {
+  setSidebarFocusable(true);
+});
+document.querySelector('.menuCloseBtn').addEventListener('click', () => {
+  setSidebarFocusable(false);
+});
   
